a95b420067a4526f5142876bfd21c91fc7c20a83
rattus-aristarchus <lankin.m.a@gmail.com>
2023-12-30 15:46:29 +0300

    pep linter for weather_tkinter

--- weather_tkinter/__main__.py
+++ weather_tkinter/__main__.py
@@ -3,15 +3,18 @@ from weather import console_io
 from weather_geolocationdb import get_city_by_ip
 import tkinter
 
+
 def show_temperature(measurement, diff):
     message = console_io.format_message(measurement, diff)
     weather_message.set(message)
+
 
 def local_weather():
     weather.local_weather(
         get_city_by_ip=get_city_by_ip,
         show_temperature=show_temperature
     )
+
 
 root = tkinter.Tk()
 button = tkinter.Button(root, text="What's the weather like?", command=local_weather)
